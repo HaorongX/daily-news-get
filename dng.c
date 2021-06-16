e720be776d51ccdf0fd5f6019cc026fbfcb71567
BeginnerC <UNYouth@hotmail.com>
2021-06-17 01:12:16 +0800

    improve help file and improve the dng.c code

--- dng.c
+++ dng.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#define FILE_VERSION "version.txt"
+#define FILE_README "readme.txt"
+#define FILE_HELP FILE_README
 void file_echo(char* name)
 {
 	FILE* file = NULL;
@@ -22,14 +25,33 @@ int main(int argc,char* argv[])
 {
 	if(1 == argc)/* Without arguments */
 	{
-		file_echo("version.txt");
-		file_echo("readme.txt");	
+		file_echo(FILE_VERSION);
+		file_echo(FILE_README);	
 	}
 	else
 	{
-		if(0 == strcmp(argv[1],"-v"))
+		switch(argc)
 		{
-			
+			case 2:/*-v --help -list -all -name*/
+				{
+					if(!strcmp(argv[1],"-v"))
+					{
+						file_echo(FILE_VERSION);	
+					}
+					else if(!strcmp(argv[1],"--help"))
+					{
+						file_echo(FILE_HELP);
+					}
+					break;
+				}
+			case 3:/* remove open close -name */
+				{
+					break;
+				}
+			default:/* install update -name */
+				{
+					break;
+				}
 		}
 	}
 	return 0;
