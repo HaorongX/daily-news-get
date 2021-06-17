578fcc9a9b455adf1591721b18123626d155b12a
BeginnerC <UNYouth@hotmail.com>
2021-06-17 20:50:22 +0800

    Add the function of get the script name

---
+++ README.txt
@@ -0,0 +1,49 @@
+DailyNewsGet App:
+1.the structure:
+DailyNewsGet
+|
+|--saves
+|--installed
+|----scripts
+|------script_name
+|------file_result(rules : Date[Year-Month-Day for example:2021-Jun-15]|website|TITLE)
+|--logs
+|--dng.out
+|--dng.c
+|--readme.txt
+|--version.txt
+2.usage age:
+dng [opnion]
+	wituout arguments:
+		equal to "--help" with "-v"
+	with arguments:
+		-v print version
+		--help give some suggestion
+		install [arguments]
+			install a script
+			for example:
+			1. dng install name1 "/home/test.pl" "perl "
+			2. dng install name2 "/home/test.py" "python "
+			3. dng install name3 "/home/test/test.pl 10" "perl "
+			Note:
+			If the script exists,its status will turn to 'open'
+			(Equal to 'open')
+			So if you want to update your script,a better way is to use the argument 'update'.	
+		remove:
+			uninstall a script
+			for example:
+			1. dng uninstall name1
+			Note:
+			If you just want to stop using the script.
+			A better argument is 'close'
+		update [arguments]
+			update a script
+			for exampe:
+			1. dng update name  "/home/test.pl"
+		open [name]
+			Turn a script status into 'open'
+		close [name]
+			Turn a script status into 'close'
+		-list list all the scripts you have installed.
+		-all use all the scripts to get the news
+		-name1 -name2 .... use the script you desing to get the news.
--- dng.c
+++ dng.c
@@ -1,12 +1,67 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #define FILE_VERSION "version.txt"
 #define FILE_README "readme.txt"
-#define FILE_HELP FILE_README
-void file_echo(char* name)
+#define FILE_HELP "helpmsg.txt"
+#define FILE_EXIST (1)
+#define FILE_UNEXIST (0)
+#define MAX_BUFFER (1024)
+#define DEBUG
+int getcharpositionbehind(char *string,char c)
 {
-	FILE* file = NULL;
+	int position = strlen(string);
+	if(position <= 0)
+	{
+		return -1;
+	}
+	while(string[position] != c)
+	{
+		position--;
+	}
+	return position;
+}
+void getscriptname(char *dest,char *source)
+{
+	int position = getcharpositionbehind(source,'/');
+	int length = strlen(source) - position;
+	#ifdef DEBUG
+		printf("position = %d\n",position);
+		printf("length = %d\n",length);
+	#endif
+	if(source[0] == '\'')
+	{
+		length--;
+	}
+	strncpy(dest,(source + position) + 1,length);
+	#ifdef DEBUG
+		puts("source:");
+		puts(source);
+		puts("dest:");
+		puts(dest);
+	#endif
+	return ;
+}
+int file_exist(char *name)
+{
+	FILE* fp = fopen(name,"r");
+	if(NULL == fp)
+	{
+		return FILE_UNEXIST;
+	}
+	else
+	{
+		fclose(fp);
+		return FILE_EXIST;
+	}
+	return 0;
+}
+void file_echo(char *name)
+{
+	FILE *file = NULL;
 	int c = 0;
 	file = fopen(name,"r");
 	if(NULL == file)
@@ -21,12 +76,12 @@ void file_echo(char* name)
 	}
 	return ;
 }
-int main(int argc,char* argv[])
+int main(int argc,char *argv[])
 {
 	if(1 == argc)/* Without arguments */
 	{
 		file_echo(FILE_VERSION);
-		file_echo(FILE_README);	
+		file_echo(FILE_HELP);	
 	}
 	else
 	{
@@ -50,6 +105,50 @@ int main(int argc,char* argv[])
 				}
 			default:/* install update -name */
 				{
+					if(!strcmp(argv[1],"install"))
+					{
+						if(!file_exist(argv[2]))
+						{
+							FILE *fp = fopen("record.dnf.ini","w");
+							if(NULL == fp)
+							{
+								puts("Fail:Create record file");
+								exit(-1);
+							}
+							{
+								char dirname[MAX_BUFFER] ="./installed/";
+								strcat(dirname,argv[2]);
+								if(mkdir(dirname,0777))
+								{
+									puts("Fail:Create Floder");
+									exit(-1);
+								}
+							}
+							{
+								char filednfcallsname[MAX_BUFFER] = "";
+								FILE *dnfcalls = NULL;
+								sprintf(filednfcallsname,"./installed/%s/.dnfcalls",argv[2]);
+								#ifdef DEBUG
+									puts(filednfcallsname);
+								#endif
+								if(!(dnfcalls = fopen(filednfcallsname,"w")))
+								{
+									puts("Fail:Create dnfcall file");
+									exit(-1);
+								}
+								{
+									int i = 0;
+									while((++i) < argc)
+									{
+										#ifdef DEBUG
+											puts(argv[i]);
+										#endif
+									}
+								}
+							}
+							
+						}
+					}
 					break;
 				}
 		}
