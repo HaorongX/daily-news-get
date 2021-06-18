89b26d42b57e930cde9a57da52981c27e560ed4f
SnailQWQ <cccxhr@yandex.com>
2021-06-18 11:58:39 +0800

    backup...

--- scripts/gmw.py
+++ scripts/gmw.py
@@ -26,4 +26,26 @@ for i in index_html:
             index.append(base_url+i[8:-1])
         else:
             index.append(base_url+i[6:-1])
-print(index)
+# 完成一级跳转
+
+head = {'User-Agent': \
+'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36'}
+num = 1
+for i in index:
+    for j in range(0,2):
+        os.system(f"curl {str(i)} > temp.html")
+    tempin = open("temp.html", mode='r')
+    temple = tempin.read()
+    tempin.close()
+    article_html = \
+        str(BeautifulSoup(str(temple),'lxml').find_all('div', class_='text_c'))
+    # 此处的 <div class="text_c"> 是整片文章（标题作者正文）
+    # 因为我懒得把标题和正文分开就这样做了
+    ex = "<(\S*?)*[^>]*>.*?|<.*? />" # 删除所有html标签
+    article_html = re.sub(ex,"",article_html)
+    # 写入文件
+    file = open(f"gmw{num}.txt", mode='w')
+    file.write(article_html)
+    file.close()
+    num+=1
+# os.system("rm temp.html")
