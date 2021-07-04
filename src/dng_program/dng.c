3e91e5b0ffae894c05ab5a9f3eae929576eb198a
BeginnerC <beginnerc@192.168.1.102>
2021-07-04 22:14:22 +0800

    Improve the code structure of dng main program

--- src/dng_program/dng.c
+++ src/dng_program/dng.c
@@ -56,6 +56,102 @@ CombineMainFunctionArguments(char *dest, int main_argc, char **main_argv, int na
     return ;
 }
 /*
+    SearchDNGConversionRelations : Search the match rule
+    Return value
+    a point to rule => success
+    NULL => failure
+*/
+ConversionRelations *
+SearchDNGConversionRelations(ConversionRelations *table, const char *command)
+{
+    ConversionRelations *temp = NULL;
+    bool flag_of_match = false;
+    temp = table -> first;
+	while(NULL != temp -> next)
+    {
+        temp = temp -> next;
+        if(0 == strcmp(temp -> current -> command, command))
+        {
+            flag_of_match = true;
+            break;
+        }
+    }
+    if(flag_of_match)
+    {    
+        return temp;
+    }
+    return NULL;
+}
+/*
+    ConventDNGCommandFileContentToMemory : Load the content to memory
+    Return value
+    a point to date table => success
+    NULL => failure
+*/
+ConversionRelations *
+ConventDNGCommandFileContentToMemory(FILE *fp)
+{
+    ConversionRelations *table = NULL;
+    char command[16] = {0};
+	char program_full_path[512] = {0};
+    /* From file content to table */
+    if(NULL == (table = (ConversionRelations *)calloc(sizeof(struct _ConversionRelations), 1)))
+	{
+		return NULL;
+	}
+	table -> first = (struct _ConversionRelations *)table; /* That is itself */
+	while(2 == fscanf(fp, "[record]\n%s => %s\n", command, program_full_path))/* Avoid empty file */
+	{
+        table -> current = (struct _ConversionRelations *)table; /* itself */
+        sprintf(table -> current -> command, "%s", command);
+        sprintf(table -> current -> program_full_path, "%s", program_full_path);
+        table -> next = (struct _ConversionRelations *)calloc(sizeof(struct _ConversionRelations), 1);
+        table -> next -> first = table -> first;
+        table = (ConversionRelations *)table -> next;
+	}
+    return table;
+}
+/*  FreeConversionRelationsMemory : Free the resource it used 
+    Return value
+    void
+*/
+void
+FreeConversionRelationsMemory(ConversionRelations *table)
+{
+    ConversionRelations *temp = NULL, *temp1 = NULL;
+    temp = table -> first;
+	while(NULL != temp)
+    {
+        temp1 = temp -> current -> next;
+        free(temp -> current);
+        temp = temp1;
+    }
+    temp = NULL;
+    table = NULL;
+    return ;
+}
+/*
+    DNGCommandParse : Parse the command and execute
+    Return value
+    1 => match
+    0 => nothing matched
+*/
+int
+DNGCommandParse(ConversionRelations *table, const char *command, const char *arguments)
+{
+    ConversionRelations *rule = NULL;
+    char final_command[1024] = {0};
+    /* Search the matched rule */
+    rule = SearchDNGConversionRelations(table, command);
+    if(NULL == rule)
+    {
+        return 0;
+    }
+    sprintf(final_command,"%s %s", rule -> current -> program_full_path, arguments);
+    system(final_command);
+    return 0;
+}
+/*
 	LoadDNGCommandFile : Load the .dngcommand file to program
 	Load in readonly mode
 	Return value
@@ -66,6 +162,17 @@ FILE*
 LoadDNGCommandFile(const char *name)
 {
 	return fopen(name, "r");
+}
+/*
+    CloseDNGCommandFile : Free the .dngcommand file resource
+    Return value
+    void
+*/
+void
+CloseDNGCommandFile(FILE *fp)
+{
+    fclose(fp);
+    fp = NULL;
 }
 /*
     main : The main part of the program
@@ -81,10 +188,9 @@ int
 main(int argc, char **argv)
 {
     FILE *dng_command_file = NULL;
-	ConversionRelations *table = NULL, *temp = NULL, *temp1 = NULL;
-	char command[16] = {0};
-	char program_full_path[512] = {0};
+	ConversionRelations *table = NULL;
     bool flag_of_match = false;
+    char arguments[1024] = {0};
     if(0 != InitWorkEnviroment())
     {
         puts("Error: Initiative failed!");
@@ -102,62 +208,22 @@ main(int argc, char **argv)
         puts("Error : Can't load the dng command file");
         exit(-1);
     }
-    /* From file content to table */
-    if(NULL == (table = (ConversionRelations *)calloc(sizeof(struct _ConversionRelations), 1)))
-	{
-		puts("Error: Can't allow memory");
-		fclose(dng_command_file);
-		dng_command_file = NULL;
-		exit(-1);
-		return -1;
-	}
-	table -> first = (struct _ConversionRelations *)table; /* That is itself */
-	while(2 == fscanf(dng_command_file, "[record]\n%s => %s\n", command, program_full_path))/* Avoid empty file */
-	{
-        table -> current = (struct _ConversionRelations *)table; /* itself */
-        sprintf(table -> current -> command, "%s", command);
-        sprintf(table -> current -> program_full_path, "%s", program_full_path);
-        table -> next = (struct _ConversionRelations *)calloc(sizeof(struct _ConversionRelations), 1);
-        table -> next -> first = table -> first;
-        table = (ConversionRelations *)table -> next;
-	}
+    /* Analyse the file content */
+    if(NULL == (table = ConventDNGCommandFileContentToMemory(dng_command_file)))
+    {
+        puts("Error : Can't analyse the file content");
+        exit(-1);
+    }
 	/* Free the file resource */
-    fclose(dng_command_file);
-    dng_command_file = NULL;
-    /* Search the command and execute it */
-	temp = table -> first;
-	while(NULL != temp -> next)
-    {
-        temp = temp -> next;
-        if(0 == strcmp(temp -> current -> command, argv[1]))
-        {
-            flag_of_match = true;
-            break;
-        }
-    }
+    CloseDNGCommandFile(dng_command_file);
     /* Parse the command */
-    if(flag_of_match)
-    {
-        char arguments[1024] = {0};
-        char command[1024] = {0};
-        CombineMainFunctionArguments(arguments, argc, argv, 3);
-        sprintf(command,"%s %s", temp -> current -> program_full_path, arguments);
-        system(command);
-    }
-    else
+    CombineMainFunctionArguments(arguments, argc, argv, 3);
+    if(0 == DNGCommandParse(table, argv[1], arguments))
     {
         printf("Sorry, can't match the command.\n");
         printf("Try 'dng --help' for more information.\n");
     }
     /* Free the memory resource */
-    temp = table -> first;
-	while(NULL != temp)
-    {
-        temp1 = temp -> current -> next;
-        free(temp -> current);
-        temp = temp1;
-    }
-    temp = NULL;
-    table = NULL;
+    FreeConversionRelationsMemory(table);
     return 0;
 }
