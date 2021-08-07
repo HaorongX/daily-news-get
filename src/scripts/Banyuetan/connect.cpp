0230951529a98d86dfd08956e5baeea1dabdc71c
SnailQWQ <cccxhr@yandex.com>
2021-08-07 12:12:28 +0800

    We Don't Need Path

--- src/scripts/Banyuetan/connect.cpp
+++ src/scripts/Banyuetan/connect.cpp
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <string>
+#include<iostream>
 
 using namespace std;
 
 int
 main(int argc, char* argv[])
 {
-    string command = "python3 ";
-    command += argv[1];
-    command += "/banyuetan.py ";
-    for (int i = 2;i < argc;++ i)
+    string command = "python3 ./banyuetan.py ";
+    for (int i = 1;i < argc;++ i)
     {
         command += argv[i];
         command += ' ';
     }
+    cout<<command<<endl;
     system(command.c_str());
     return 0;
 }
