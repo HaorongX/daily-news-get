8aa073fe636b16c848f2edb17e336d71aba7bbb5
SnailQWQ <cccxhr@yandex.com>
2021-08-15 13:44:48 +0800

    完善 NPR 爬虫

--- src/package/NPR/npr.py
+++ src/package/NPR/npr.py
@@ -1,10 +1,13 @@
 # Portions Copyright 2021 (c) Public Domain
 
+from typing import DefaultDict
 import requests
 import lxml
 from bs4 import BeautifulSoup
 import os
+import sys
 import  re
+import subprocess
 import time
 
 def get_news_list(base_url,headers):
@@ -14,23 +17,29 @@ def get_news_list(base_url,headers):
 
     news_list = str(news_list_page_html.find_all("h3",class_="rundown-segment__title"))
 
-    news_url=[]
+    news_url= ""
     news_list=news_list.split('"')
     for i in news_list:
         if i[0:4]=="http":
-            # print(i,"\n")
             i = i.replace("\\","")
-            news_url.append(i)
+            news_url = i
+            break
     return news_url
 
 def get_news(news_url):
     news = requests.get(news_url)
     news.encoding = "utf-8"
     news_page_details = BeautifulSoup(news.text,'lxml')
-    news_title_html = str(news_page_details.find_all('h1'))
-    news_html = str(news_page_details.find_all('div', class_='storytext storylocation linkLocation'))
-    news_html += str(news_page_details.find_all('div', class_='transcript storytext'))
-    return {"title":news_title_html,"news":news_html}
+    news_title_html = str(news_page_details.find('h1'))
+    news_html = str(news_page_details.find('div', class_='storytext storylocation linkLocation'))
+    news_html += str(news_page_details.find('div', class_='transcript storytext'))
+    mp3_html = str(news_page_details.find('li',class_="audio-tool audio-tool-download"))
+    mp3_url = ""
+    for i in mp3_html.split('"'):
+        if i.find("http") != -1:
+            mp3_url = i
+            break
+    return {"title":news_title_html,"news":news_html,"mp3":mp3_url}
 
 # 该函数用于删除字符串中的所有html标签
 def delete_html_tag(string):
@@ -39,7 +48,7 @@ def delete_html_tag(string):
 
 # 将新闻写入文件
 def write_into_file(file_name,title,article):
-    file = open(file_name,"w")
+    file = open(sys.path[0]+"/result/"+file_name,"w+")
     file.write(title)
     file.write("\n")
     file.write(article)
@@ -47,16 +56,27 @@ def write_into_file(file_name,title,article):
 
 # ----------以下为主程序----------
 
+os.system("rm -rf result")
+os.mkdir("result")
+
 header = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"}
 
+print("获取新闻地址......")
 news_list_url = "https://www.npr.org/programs/morning-edition/"
 news_url = get_news_list(news_list_url,header)
+print("完成！")
 
-for i in news_url:
-    news = get_news(i)
-    # 删除标签及正文中的html标签
-    news["title"] = delete_html_tag(news["title"])
-    news["news"] = delete_html_tag(news["news"])
-    filename = time.strftime("%Y-%m-%d", time.localtime())+"\\NPR\\"+news["title"]+".txt"
-    write_into_file(filename, news["title"], news["news"])
- 
+print("获取文章......")
+news = get_news(news_url)
+print("完成！")
+# 删除标签及正文中的html标签
+news["title"] = delete_html_tag(news["title"])
+news["news"] = delete_html_tag(news["news"])
+filename = time.strftime("%Y-%m-%d", time.localtime())+"\\NPR\\"+news["title"]+".txt"
+write_into_file(filename, news["title"], news["news"])
+filename = time.strftime("%Y-%m-%d", time.localtime())+"\\NPR\\"+news["title"]+".mp3"
+print(filename)
+print("运行 wget ......")
+subprocess.run(["wget", "-nv", f"-O{filename}", f"{news['mp3']}"])
+print("完成")
+subprocess.run(["mv", "-f", f"{sys.path[0]}/{filename}", f"{sys.path[0]}/result"])
